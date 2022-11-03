fef1d9fc436e187d6c66c47b907b2f30bd2547d1
Yuri Gabriel <67286222+yurigabriel25@users.noreply.github.com>
2022-11-02 21:38:57 -0300

    Versão final do programa

--- prototipoPecasAutomotivas.py
+++ prototipoPecasAutomotivas.py
@@ -1,35 +1,42 @@
 from calendar import c
 from operator import pos
 
-# Código em desenvolvimento.
+# Script simples para resolver problema de trabalho de faculdade.
 
 # Trabalho de faculdade, Intrução ao Python
-# Criar um código que preveja a quantidade de peças que serão vendidas em um determinado mês com base nas vendas atuais
+# Criar um código que preveja a quantidade de peças que serão vendidas em um determinado mês com base nas vendas apresentadas em gráfico.
 
-c = 200
+# Dados de peças vendidas nos últimos meses salvos em variaveis.
+janeiro = 200
+fevereiro = 400
+marco = 600
+abril = 800
+maio = 1000
+junho = 1200
 
-# Dados de peças vendidas nos últimos meses
-jan = ' janeiro = ', 200
-fev = ' fevereiro = ', 400
-mar = ' março = ', 600
-abril = ' abril = ', 800
-maio = ' maio = ', 1000
-jun = ' juho = ', 1200
+print('Para qual mês quer prever as vendas? ')
 
-vendas = [jan,fev,mar,abril,maio,jun]
+proxMes = input('Digite o número referente ao mês: ') # Entrada do dado do mês escolhido.
 
-
-ultimasVendas = input("Deseja ver os números das ultimas vendas de peças? ")
-
-if ultimasVendas == 'Sim' or ultimasVendas == 'sim' or ultimasVendas == 's': # Assegura ao input que todas as opções de respostas positivas sejam aceitas.
-    print(vendas) # Informa os dados de vendas anteriores.
-
-    print('Para qual mês quer prever as vendas? ')
-    proxMes = input('Digite o número referente ao mês: ')
-    r = proxMes * c
-    print(f'O mês {proxMes} vai ter uma estimativa de {r} peças.')
-else:
-    print('Para qual mês quer prever as vendas? ')
-    proxMes = input('Digite o número referente ao mês: ')
-    r2 = proxMes * c
-    print(f'O mês {proxMes} vai ter uma estimativa de {r2} peças.')
+# if e else if para comparar o mês digitado com o dado referente ao mês salvo nas variáveis.
+if proxMes == 'janeiro' or proxMes == 'Janeiro':
+    print(f'No mês de janeiro foram vendidas {janeiro} peças')
+    input('Digite alguma tecla para sair...')
+elif proxMes == 'fevereiro' or proxMes == 'Fevereiro':
+    print(f'No mês de fevereiro foram vendidas {fevereiro} peças.')
+    input('Digite alguma tecla para sair...')
+elif proxMes == 'março' or proxMes == 'Março':
+    print(f'No mês de março foram vendidas {marco} peças')
+    input('Digite alguma tecla para sair...')
+elif proxMes == 'abril' or proxMes == 'Abril':
+    print(f'No mês de março foram vendidas {abril} peças')
+    input('Digite alguma tecla para sair...')
+elif proxMes == 'maio' or proxMes == 'Maio':
+    print(f'No mês de março foram vendidas {maio} peças')
+    input('Digite alguma tecla para sair...')
+elif proxMes == 'junho' or proxMes == 'Junho':
+    print(f'No mês de março foram vendidas {junho} peças')
+    input('Digite alguma tecla para sair...')
+else: # Fim do programa.
+    print('Nenhum mês foi selecionado.')
+    input('Digite alguma tecla para sair...')
